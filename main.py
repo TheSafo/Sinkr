e1c3e1d2fb1c3c5d83ebb16e5a8369a38795e1b5
Jacob Gold <goldjacobe@gmail.com>
2016-10-23 05:00:31 -0400

    Done with major database work, committing for testing.

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ def main():
     # dicts for db
     cups = {i:{'x':x, 'y':y} for i in range(0, 6) for (x,y) in cupLocations(cupsleft)}
     players = {x:'' for x in range(0,2)}
+    scores = {x:0 for x in range(0,2)}
 
     # initial db setup
     fb.put('/games/' + str(gameId), 'cups', cups)
     fb.put('/games/' + str(gameId), 'players', players)
+    fb.put('/games/' + str(gameId), 'scores', scores)
     fb.put('/games/' + str(gameId), 'turn', turn)
 
     # wait for players to connect
@@ -26,7 +28,15 @@ def main():
         sleep(1)
 
     # main loop
-   # while (len(cups) > 0):
+    while (len(cups) > 0):
+        if throwBall(cupsleft) != (None, None):
+            cupsleft -= 1
+            scores[turn] += 1
+            turn ^= 1
+            cups = {i:{'x':x, 'y':y} for i in range(0, 6) for (x,y) in cupLocations(cupsleft)}
+            fb.put('/games/' + str(gameId), 'cups', cups)
+            fb.put('/games/' + str(gameId), 'scores', scores)
+            fb.put('/games/' + str(gameId), 'turn', turn)
 
 
 if __name__ == '__main__':
